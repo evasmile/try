f552a9ccf14455e140903f114c86445c8b7babb7
surprise_Mathonsi <surprisehlawutelo@gmail..com>
2024-03-07 10:48:14 +0200

    initial commit

--- task1.js
+++ task1.js
@@ -1,6 +1,6 @@
-function addNumbers(num1 , num2){
-  var total =  num1 + num2 ;
+function addNumbers(num1 , num2 , num3){
+  var total =  num1 + num2 + num3 ;
   console.log(total);
 }
 
-addNumbers(2,3)
+addNumbers(2,3,1)
